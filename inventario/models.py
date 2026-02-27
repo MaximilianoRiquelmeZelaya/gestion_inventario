6b14c76646169439376bdfe26f9b901785158c9f
Maximiliano Riquelme Zelaya <maximilianoriquelme@yahoo.com>
2026-02-27 14:51:41 -0300

    Add files via upload

--- inventario/models.py
+++ inventario/models.py
@@ -7,12 +7,11 @@ class Usuario(AbstractUser):
     rol = models.CharField(max_length=15, choices=ROLE_CHOICES, default='user')
     permisos_colecciones = models.JSONField(default=list, blank=True)
     
-    # NUEVAS PREFERENCIAS DE USUARIO
-    tema = models.CharField(max_length=20, default='system') # light, dark, system
+    tema = models.CharField(max_length=20, default='system')
     pagina_inicio = models.CharField(max_length=50, default='inventario')
     db_preferida = models.CharField(max_length=100, blank=True, null=True)
     bodega_preferida = models.CharField(max_length=100, blank=True, null=True)
-    orden_formularios = models.JSONField(default=dict, blank=True) # Guarda el orden de los campos al crear/editar
+    orden_formularios = models.JSONField(default=dict, blank=True)
 
     def puede_editar(self, coleccion):
         if self.rol in ['admin', 'superuser']:
@@ -36,7 +35,6 @@ class Solicitud(models.Model):
     solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='solicitudes')
     insumo = models.CharField(max_length=200)
     
-    # NUEVOS CAMPOS: Especifican la DB exacta
     db_origen = models.CharField(max_length=100, default='-')
     bodega_origen = models.CharField(max_length=100)
     
@@ -52,7 +50,7 @@ class RegistroIntegridad(models.Model):
     fecha_monitoreo = models.DateTimeField(default=timezone.now)
     nombre_monitor = models.CharField(max_length=200)
     ubicacion = models.CharField(max_length=200, blank=True, null=True)
-    equipo = models.CharField(max_length=200) # Corresponde al Producto
+    equipo = models.CharField(max_length=200)
     cantidad = models.IntegerField(default=0)
     estatus = models.CharField(max_length=2, choices=[('C', 'C'), ('NC', 'NC')], blank=True, null=True)
     accion_correctiva = models.TextField(blank=True, null=True)
@@ -60,7 +58,6 @@ class RegistroIntegridad(models.Model):
     verificacion = models.TextField(blank=True, null=True)
     estado_verificacion = models.CharField(max_length=50, default='Pendiente de verificación')
     
-    # Metadatos para sincronización con MongoDB
     mongo_doc_id = models.CharField(max_length=50)
     db_origen = models.CharField(max_length=100)
     coll_origen = models.CharField(max_length=100)
@@ -86,7 +83,7 @@ class HistorialCambio(models.Model):
     hora = models.TimeField(auto_now_add=True)
     base = models.CharField(max_length=100, default='-')
     coleccion = models.CharField(max_length=100, default='-')
-    datos_extra = models.JSONField(blank=True, null=True) # Para guardar el JSON del producto borrado
+    datos_extra = models.JSONField(blank=True, null=True)
 
     class Meta:
         ordering = ['-fecha', '-hora']
